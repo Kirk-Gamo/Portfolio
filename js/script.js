41a0dbebe1babb30b1a4a1190f06790b513f802d
Kirk-Gamo <146793945+Kirk-Gamo@users.noreply.github.com>
2023-12-18 18:27:33 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+/*=============== Toggle Icon Navbar ================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+
+/*=============== Scroll Sections Active Link ================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec. offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset  && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active'); 
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+
+    /*=============== Sticky Nav Bar ================*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', scrollY > 100);
+
+
+    /*=============== Remove toggle icon and navbar when clicked navbar link (scroll) ================*/
+    menuIcon.classList.remove('bx-x')
+    navbar.classList.remove('active')
+};
+
+
+/*=============== Scroll Reveal ================*/
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200 
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .skills-container, .certificates-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/*=============== Typed JS ================*/
+const type = new Typed('.multiple-text', {
+    strings: ['Front-end Developer', 'Leader', 'Student', 'Novel Reader', 'Gamer', 'Lover'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
